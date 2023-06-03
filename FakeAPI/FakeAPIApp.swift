e92b0a2ffcf7de61a1828c6645e977de142bf62c
mariam <mariiamahmed11@hotmail.com>
2023-06-04 02:28:44 +0300

    Initial Commit

---
+++ FakeAPI/FakeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAPIApp.swift
+//  FakeAPI
+//
+//  Created by user on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FakeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
